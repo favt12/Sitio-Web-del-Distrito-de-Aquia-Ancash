f09f1cbda0c065212649c6d5a8d3edf5013cf829
favt12 <alexis_22_95@hotmail.com>
2018-12-02 01:18:07 -0500

    Se migro la clase CSS .title-contenedor al arch.principal(main.css) como clase global para los titulos primarios

--- includes/templates/lugares-turisticos.php
+++ includes/templates/lugares-turisticos.php
@@ -135,7 +135,7 @@
     </div><!-- .carousel .slide #slider -->
   </div><!-- .slider -->
 
-  <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
+  <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
     <h3>Templo del Señor de Cayac</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
