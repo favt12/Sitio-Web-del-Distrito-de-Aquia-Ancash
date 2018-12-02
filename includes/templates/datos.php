6f488cc1094717de8a1b0fe245acfe7355966f18
favt12 <alexis_22_95@hotmail.com>
2018-12-02 01:23:13 -0500

    Se agregaron el arch. datos.php y datos.css al proyecto

---
+++ includes/templates/datos.php
@@ -0,0 +1,13 @@
+<section>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="title-contenedor col-12 text-center">
+
+          <h2>DATOS</h2>
+
+
+      </div>
+    </div>
+
+  </div>
+</section>
