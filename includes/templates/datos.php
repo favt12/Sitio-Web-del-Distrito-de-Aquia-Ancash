a70e4c6a59230be380dc92ae84f2e2aa23ef6989
favt12 <alexis_22_95@hotmail.com>
2018-12-03 16:02:45 -0500

    Se avanzo la estructura de la seccion datos utiles Y se le dieron algunos estilos CSS

--- includes/templates/datos.php
+++ includes/templates/datos.php
@@ -1,13 +1,42 @@
 <section>
   <div class="container-fluid">
     <div class="row">
-      <div class="title-contenedor col-12 text-center">
-
-          <h2>DATOS</h2>
-
-
+      <div class="title-contenedor title-contenedor-date col-12 text-center">
+        <h2>DATOS UTILES</h2>
       </div>
     </div>
 
-  </div>
+    <div class="row">
+
+      <div class="data-content-right col-2 d-flex justify-content-start">
+        <div class="btn-group" role="group" aria-label="First group">
+          <div class="data-content-btn btn-group-vertical nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="btn btn-secondary mb-4 nav-item nav-link active" id="nav-calendar-tab" data-toggle="tab" href="#nav-calendar" role="tab" aria-controls="nav-calendar" aria-selected="true"><i class="icon-calendar"></i></a>
+            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-movil-tab" data-toggle="tab" href="#nav-movil" role="tab" aria-controls="nav-movil" aria-selected="false"><i class="icon-taxi"></i></a>
+            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-ambulance-tab" data-toggle="tab" href="#nav-ambulance" role="tab" aria-controls="nav-ambulance" aria-selected="false"><i class="icon-ambulance"></i></a>
+          </div>
+      </div>
+    </div>
+
+      <div class="data-content-center col-8">
+        <div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-calendar" role="tabpanel" aria-labelledby="nav-calendar-tab">...</div>
+  <div class="tab-pane fade" id="nav-movil" role="tabpanel" aria-labelledby="nav-movil-tab">fgff</div>
+  <div class="tab-pane fade" id="nav-ambulance" role="tabpanel" aria-labelledby="nav-ambulance-tab">dxdxdxd</div>
+  <div class="tab-pane fade" id="nav-money" role="tabpanel" aria-labelledby="nav-money-tab">ohohoh</div>
+</div>
+      </div>
+
+      <div class="data-content-left col-2 d-flex justify-content-end">
+        <div class="btn-group" role="group" aria-label="First group">
+          <div class="data-content-btn btn-group-vertical">
+            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-money-tab" data-toggle="tab" href="#nav-money" role="tab" aria-controls="nav-money" aria-selected="false"><i class="icon-calendar"></i></a>
+            <a class="btn btn-secondary my-4"><i class="icon-money"></i></a>
+            <a class="btn btn-secondary my-4"><i class="icon-money"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </div><!-- .row -->
+  </div><!-- .container-fluid -->
 </section>
