a89e097884a01402cd77088b9396d4ed64250f23
favt12 <alexis_22_95@hotmail.com>
2018-12-03 23:13:46 -0500

    Se hicieron algunos cambios -Se cambiaron los icon- por img para los button del arch.datos.php y se le dieron estilos CSS

--- includes/templates/datos.php
+++ includes/templates/datos.php
@@ -1,38 +1,41 @@
 <section>
   <div class="container-fluid">
     <div class="row">
-      <div class="title-contenedor title-contenedor-date col-12 text-center">
+      <div id="datos" class="title-contenedor title-contenedor-date col-12 text-center">
         <h2>DATOS UTILES</h2>
       </div>
     </div>
 
     <div class="row">
 
-      <div class="data-content-right col-2 d-flex justify-content-start">
+      <div class="data-content-right col-3 col-md-2 d-flex justify-content-start">
         <div class="btn-group" role="group" aria-label="First group">
           <div class="data-content-btn btn-group-vertical nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="btn btn-secondary mb-4 nav-item nav-link active" id="nav-calendar-tab" data-toggle="tab" href="#nav-calendar" role="tab" aria-controls="nav-calendar" aria-selected="true"><i class="icon-calendar"></i></a>
-            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-movil-tab" data-toggle="tab" href="#nav-movil" role="tab" aria-controls="nav-movil" aria-selected="false"><i class="icon-taxi"></i></a>
-            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-ambulance-tab" data-toggle="tab" href="#nav-ambulance" role="tab" aria-controls="nav-ambulance" aria-selected="false"><i class="icon-ambulance"></i></a>
-          </div>
+            <a class="btn btn-secondary mb-2 nav-item nav-link active rounded-circle" id="nav-calendar-tab" data-toggle="tab" href="#nav-calendar" role="tab" aria-controls="nav-calendar" aria-selected="true"><img src="img/button_logo_calendar.png" alt=""></a>
+            <a class="btn btn-dark my-2 nav-item nav-link rounded-circle" id="nav-movil-tab" data-toggle="tab" href="#nav-movil" role="tab" aria-controls="nav-movil" aria-selected="false"><img src="img/button_logo_autobus.png" alt=""></a>
+            <a class="btn btn-warning my-2 nav-item nav-link rounded-circle" id="nav-ambulance-tab" data-toggle="tab" href="#nav-ambulance" role="tab" aria-controls="nav-ambulance" aria-selected="false"><img src="img/button_logo_cruzroja.png" alt=""></a>
+            <a class="btn btn-success my-2 nav-item nav-link rounded-circle" id="nav-dollar-tab" data-toggle="tab" href="#nav-dollar" role="tab" aria-controls="nav-dollar" aria-selected="false"><img src="img/button_logo_moneda.png" alt=""></a>
+            <a class="btn btn-light my-2 nav-item nav-link rounded-circle" id="nav-clima-tab" data-toggle="tab" href="#nav-clima" role="tab" aria-controls="nav-clima" aria-selected="false"><img src="img/button_logo_clima.png" alt=""></a>
+          </div><!-- .data-content-right .col-3 .col-md-2 .d-flex .justify-content-start -->
       </div>
     </div>
 
-      <div class="data-content-center col-8">
+      <div class="data-content-left col-9 col-md-10">
         <div class="tab-content" id="nav-tabContent">
-  <div class="tab-pane fade show active" id="nav-calendar" role="tabpanel" aria-labelledby="nav-calendar-tab">...</div>
-  <div class="tab-pane fade" id="nav-movil" role="tabpanel" aria-labelledby="nav-movil-tab">fgff</div>
-  <div class="tab-pane fade" id="nav-ambulance" role="tabpanel" aria-labelledby="nav-ambulance-tab">dxdxdxd</div>
-  <div class="tab-pane fade" id="nav-money" role="tabpanel" aria-labelledby="nav-money-tab">ohohoh</div>
-</div>
-      </div>
-
-      <div class="data-content-left col-2 d-flex justify-content-end">
-        <div class="btn-group" role="group" aria-label="First group">
-          <div class="data-content-btn btn-group-vertical">
-            <a class="btn btn-secondary my-4 nav-item nav-link" id="nav-money-tab" data-toggle="tab" href="#nav-money" role="tab" aria-controls="nav-money" aria-selected="false"><i class="icon-calendar"></i></a>
-            <a class="btn btn-secondary my-4"><i class="icon-money"></i></a>
-            <a class="btn btn-secondary my-4"><i class="icon-money"></i></a>
+          <div class="tab-pane fade show active" id="nav-calendar" role="tabpanel" aria-labelledby="nav-calendar-tab">
+            ...
+          </div>
+          <div class="tab-pane fade" id="nav-movil" role="tabpanel" aria-labelledby="nav-movil-tab">
+            fgff
+          </div>
+          <div class="tab-pane fade" id="nav-ambulance" role="tabpanel" aria-labelledby="nav-ambulance-tab">
+            dxdxdxd
+          </div>
+          <div class="tab-pane fade" id="nav-dollar" role="tabpanel" aria-labelledby="nav-dollar-tab">
+            ohohoh
+          </div>
+          <div class="tab-pane fade" id="nav-clima" role="tabpanel" aria-labelledby="nav-clima-tab">
+            jhkjukjkhj
           </div>
         </div>
       </div>
