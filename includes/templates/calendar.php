2dd63f21b5d1ba0317ee67b8929937684653df94
favt12 <alexis_22_95@hotmail.com>
2018-12-05 03:03:35 -0500

    se Avanzo el arch. calendar.php y se le dieron algunos estilos CSS

---
+++ includes/templates/calendar.php
@@ -0,0 +1,22 @@
+<div class="row text-muted">
+  <div class="col-12"><h3 class="my-3 text-center text-md-left">DIAS FESTIVOS</h3></div>
+    <div class="col-12 col-md-8" id="calendar"></div>
+    <div class="col-12 col-md-4 d-flex align-items-center">
+  <div class="">
+    <ul>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+      <li>hola mundo</li>
+    </ul>
+  </div>
+    </div>
+</div>
