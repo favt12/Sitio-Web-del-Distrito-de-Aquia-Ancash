0a5664b794a500265e92f1795e20e11be050c0a8
favt12 <alexis_22_95@hotmail.com>
2018-12-05 02:57:02 -0500

    Se le dieron algunos ajustes a la seccion datos utiles

--- includes/templates/datos.php
+++ includes/templates/datos.php
@@ -6,27 +6,29 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="row justify-content-between">
 
-      <div class="data-content-right col-3 col-md-2 d-flex justify-content-start">
-        <div class="btn-group" role="group" aria-label="First group">
-          <div class="data-content-btn btn-group-vertical nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="btn btn-secondary mb-2 nav-item nav-link active rounded-circle" id="nav-calendar-tab" data-toggle="tab" href="#nav-calendar" role="tab" aria-controls="nav-calendar" aria-selected="true"><img src="img/button_logo_calendar.png" alt=""></a>
-            <a class="btn btn-dark my-2 nav-item nav-link rounded-circle" id="nav-movil-tab" data-toggle="tab" href="#nav-movil" role="tab" aria-controls="nav-movil" aria-selected="false"><img src="img/button_logo_autobus.png" alt=""></a>
-            <a class="btn btn-warning my-2 nav-item nav-link rounded-circle" id="nav-ambulance-tab" data-toggle="tab" href="#nav-ambulance" role="tab" aria-controls="nav-ambulance" aria-selected="false"><img src="img/button_logo_cruzroja.png" alt=""></a>
-            <a class="btn btn-success my-2 nav-item nav-link rounded-circle" id="nav-dollar-tab" data-toggle="tab" href="#nav-dollar" role="tab" aria-controls="nav-dollar" aria-selected="false"><img src="img/button_logo_moneda.png" alt=""></a>
-            <a class="btn btn-light my-2 nav-item nav-link rounded-circle" id="nav-clima-tab" data-toggle="tab" href="#nav-clima" role="tab" aria-controls="nav-clima" aria-selected="false"><img src="img/button_logo_clima.png" alt=""></a>
+      <div class="data-content-right col-12 col-md-1 ">
+        <div class="data-content btn-group mb-5 col-12 justify-content-center" role="group" aria-label="First group">
+          <div class="data-content-btn  nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
+            <a class="btn btn-secondary mb-2 mx-2 nav-item nav-link active rounded" id="nav-calendar-tab" data-toggle="tab" href="#nav-calendar" role="tab" aria-controls="nav-calendar" aria-selected="true"><img src="img/button_logo_calendar.png" alt=""></a>
+            <a class="btn btn-dark my-2 mx-2 nav-item nav-link rounded" id="nav-movil-tab" data-toggle="tab" href="#nav-movil" role="tab" aria-controls="nav-movil" aria-selected="false"><img src="img/button_logo_autobus.png" alt=""></a>
+            <a class="btn btn-warning my-2 mx-2 nav-item nav-link rounded" id="nav-ambulance-tab" data-toggle="tab" href="#nav-ambulance" role="tab" aria-controls="nav-ambulance" aria-selected="false"><img src="img/button_logo_cruzroja.png" alt=""></a>
+            <a class="btn btn-success my-2 mx-2 nav-item nav-link rounded" id="nav-dollar-tab" data-toggle="tab" href="#nav-dollar" role="tab" aria-controls="nav-dollar" aria-selected="false"><img src="img/button_logo_moneda.png" alt=""></a>
+            <a class="btn btn-light my-2 mx-2 nav-item nav-link rounded" id="nav-clima-tab" data-toggle="tab" href="#nav-clima" role="tab" aria-controls="nav-clima" aria-selected="false"><img src="img/button_logo_clima.png" alt=""></a>
           </div><!-- .data-content-right .col-3 .col-md-2 .d-flex .justify-content-start -->
       </div>
     </div>
 
-      <div class="data-content-left col-9 col-md-10">
+      <div class="data-content-left col col-md-10">
         <div class="tab-content" id="nav-tabContent">
           <div class="tab-pane fade show active" id="nav-calendar" role="tabpanel" aria-labelledby="nav-calendar-tab">
-            ...
+            <?php require_once 'includes/templates/calendar.php' ?>
           </div>
           <div class="tab-pane fade" id="nav-movil" role="tabpanel" aria-labelledby="nav-movil-tab">
-            fgff
+            <h2>xwdsdsd</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
           <div class="tab-pane fade" id="nav-ambulance" role="tabpanel" aria-labelledby="nav-ambulance-tab">
             dxdxdxd
