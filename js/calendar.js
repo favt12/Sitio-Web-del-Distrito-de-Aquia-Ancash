1b627e4f0ef4658a8631c4be25f2e825ac1130bb
favt12 <alexis_22_95@hotmail.com>
2018-12-05 02:59:19 -0500

    Se crearon los archivos del plugin FULLCALENDAR para el proyecto

---
+++ js/calendar.js
@@ -0,0 +1,9 @@
+$(function() {
+
+  // La página ya está lista, inicializa el calendario ...
+
+  $('#calendar').fullCalendar({
+    // ponga sus opciones y devoluciones de llamada aquí
+  })
+
+});
