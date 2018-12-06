2adfe4d9762af561783a0f1297bfcd110652b4c6
favt12 <alexis_22_95@hotmail.com>
2018-12-05 23:10:32 -0500

    Se agregaron las fechas con sus eventos en el row del calendario

--- includes/templates/calendar.php
+++ includes/templates/calendar.php
@@ -4,18 +4,10 @@
     <div class="col-12 col-md-4 d-flex align-items-center">
   <div class="">
     <ul>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
-      <li>hola mundo</li>
+      <li><p><small><strong>27 de Septiembre</strong> Fiesta costumbrista a San Miguel Arcangel</small></p></li>
+      <li><p><small><strong>30 de Diciembre</strong> Fiesta honor al niño Jesùs en Año Nuevo</small></p></li>
+      <li><p><small><strong>3 de Mayo</strong> Honor al Señor de Cayac </small></p></li>
+
     </ul>
   </div>
     </div>
