8938561426725d5c5cd7b4c1c7563df1099a5e46
favt12 <alexis_22_95@hotmail.com>
2019-01-13 15:54:03 -0500

    Se agregaron los arch. footer.sass y footer.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,23 @@
+<footer>
+  <div class="container-fluid">
+    <div class="footer row text-center">
+      <div class="col-4">
+        <h4>Corporation Telvas</h4>
+        <h6 class="text-uppercase">contacto: 947391408</h6>
+
+      </div>
+      <div class="col-4">
+        <h6>COPYRIGHT © 2019</h6>
+      </div>
+      <div class="sociales col-4">
+        <a href="#"><i class="icon-facebook"></i></a>
+        <a href="#"><i class="icon-twitter"></i></a>
+        <a href="#"><i class="icon-github-circled"></i></a>
+        <a href="#"><i class="icon-instagram"></i></a>
+
+      </div>
+
+    </div>
+
+  </div>
+</footer>
