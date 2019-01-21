26fc6afbc10865d59252cb437a3b99f6cd9d2a1e
favt12 <alexis_22_95@hotmail.com>
2019-01-20 19:17:52 -0500

    Se creo el arch. comments.php para la seccion de los comentarios del page

---
+++ includes/templates/comments.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div id="fb-root"></div>
+      <div class="fb-comments" data-href="http://localhost/aquia_page_oficial/" data-numposts="5"></div>
+    </div>
+  </div>
+</div>
