930218e8cceb5a4bc7c77b1378cc08f9d8cfafa6
favt12 <alexis_22_95@hotmail.com>
2019-02-21 09:55:23 -0500

    Se agrego una nueva caja de SLIDER en el archivo lugares-turisticos.php (PLAZA DE TOROS)

--- includes/templates/lugares-turisticos.php
+++ includes/templates/lugares-turisticos.php
@@ -143,7 +143,7 @@
 
 <hr>
 
-<div class="row mb-5">
+<div class="row">
   <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
     <h3>Puya de Raymondi</h3>
     <p>Recurso natural que se encuentra en el valle de Cara en el caserio de Pachapaqui.</p>
@@ -151,59 +151,11 @@
 
   <div class="col-12 col-lg-7 slider">
 
-    <div class="carousel slide" id="slider-four" data-ride="carousel">
+    <div class="carousel slide" id="slider-seven" data-ride="carousel">
       <ol class="carousel-indicators">
-        <li data-target="#slider-four" data-slide-to="0" class="active"></li>
-        <li data-target="#slider-four" data-slide-to="1"></li>
-        <li data-target="#slider-four" data-slide-to="2"></li>
-      </ol>
-
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/aquia_hd.png" alt="imagen #1" class="d-block img-fluid" >
-        </div><!-- .carousel-item .active -->
-
-        <div class="carousel-item">
-          <img src="img/aquia_hd.png" alt="imagen #2" class="d-block img-fluid">
-        </div><!-- .carousel-item -->
-
-        <div class="carousel-item">
-          <img src="img/aquia_hd.png" alt="imagen #3" class="d-block img-fluid">
-          <div class="carousel-caption d-none d-md-block">
-            <h4>CONOCE LAS MEJORES PLAYAS</h4>
-            <p>Diversion Asegurada 100%</p>
-          </div>
-        </div><!-- .carousel-item -->
-     </div><!-- .carousel-inner -->
-
-      <a href="#slider-four" class="carousel-control-prev" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
-      </a>
-      <a href="#slider-four" class="carousel-control-next" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Siguiente</span>
-      </a>
-    </div><!-- .carousel .slide #slider -->
-  </div><!-- .slider -->
-
-</div><!-- .row -->
-
-<hr>
-
-<div class="row">
-  <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-    <h3>Laguna Cococha</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-
-  <div class="col-12 col-lg-7 slider">
-
-    <div class="carousel slide" id="slider-two" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#slider-two" data-slide-to="0" class="active"></li>
-        <li data-target="#slider-two" data-slide-to="1"></li>
-        <li data-target="#slider-two" data-slide-to="2"></li>
+        <li data-target="#slider-seven" data-slide-to="0" class="active"></li>
+        <li data-target="#slider-seven" data-slide-to="1"></li>
+        <li data-target="#slider-seven" data-slide-to="2"></li>
       </ol>
 
       <div class="carousel-inner">
@@ -224,14 +176,108 @@
         </div><!-- .carousel-item -->
      </div><!-- .carousel-inner -->
 
-      <a href="#slider-two" class="carousel-control-prev" data-slide="prev">
+      <a href="#slider-seven" class="carousel-control-prev" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Anterior</span>
       </a>
-      <a href="#slider-two" class="carousel-control-next" data-slide="next">
+      <a href="#slider-seven" class="carousel-control-next" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Siguiente</span>
       </a>
     </div><!-- .carousel .slide #slider -->
   </div><!-- .slider -->
+</div><!-- .row -->
+
+<hr>
+
+<div class="row">
+  <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
+    <h3>Laguna Cococha</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+
+  <div class="col-12 col-lg-7 slider">
+
+    <div class="carousel slide" id="slider-six" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#slider-six" data-slide-to="0" class="active"></li>
+        <li data-target="#slider-six" data-slide-to="1"></li>
+        <li data-target="#slider-six" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/cococha.jpg" alt="imagen #1" class="d-block img-fluid" >
+        </div><!-- .carousel-item .active -->
+
+        <div class="carousel-item">
+          <img src="img/cococha.jpg" alt="imagen #2" class="d-block img-fluid">
+        </div><!-- .carousel-item -->
+
+        <div class="carousel-item">
+          <img src="img/cococha.jpg" alt="imagen #3" class="d-block img-fluid">
+          <div class="carousel-caption d-none d-md-block">
+            <h4>CONOCE LAS MEJORES PLAYAS</h4>
+            <p>Diversion Asegurada 100%</p>
+          </div>
+        </div><!-- .carousel-item -->
+     </div><!-- .carousel-inner -->
+
+      <a href="#slider-six" class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a href="#slider-six" class="carousel-control-next" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div><!-- .carousel .slide #slider -->
+  </div><!-- .slider -->
+</div><!-- .row -->
+
+
+<hr>
+
+<div class="row">
+  <div class="col-12 col-lg-7 slider">
+
+    <div class="carousel slide" id="slider-nine" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#slider-nine" data-slide-to="0" class="active"></li>
+        <li data-target="#slider-nine" data-slide-to="1"></li>
+        <li data-target="#slider-nine" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/cococha.jpg" alt="imagen #1" class="d-block img-fluid" >
+        </div><!-- .carousel-item .active -->
+
+        <div class="carousel-item">
+          <img src="img/cococha.jpg" alt="imagen #2" class="d-block img-fluid">
+        </div><!-- .carousel-item -->
+
+        <div class="carousel-item">
+          <img src="img/cococha.jpg" alt="imagen #3" class="d-block img-fluid">
+          <div class="carousel-caption d-none d-md-block">
+            <h4>CONOCE LAS MEJORES PLAYAS</h4>
+            <p>Diversion Asegurada 100%</p>
+          </div>
+        </div><!-- .carousel-item -->
+     </div><!-- .carousel-inner -->
+
+      <a href="#slider-nine" class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a href="#slider-nine" class="carousel-control-next" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div><!-- .carousel .slide #slider -->
+  </div><!-- .slider -->
+  <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
+    <h3>Plaza de Toros</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
 </div><!-- .row -->
