a814bf9dc2f49d32c39714677829ef6f80d7382c
favt12 <alexis_22_95@hotmail.com>
2019-02-22 03:23:21 -0500

    Se le agrego informacion(string) a la seccion ubicacion en el archivo ubicacion.php

--- includes/templates/ubicacion.php
+++ includes/templates/ubicacion.php
@@ -10,6 +10,16 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
+        <h3 class="text-center mb-5">UBICACION DE AQUIA</h3>
+        <ul>
+          <li><p><small><strong>COORDENADAS:</strong> 10° 4′ 59″ S, 77° 7′ 59″.</small></p></li>
+          <li><p><small><strong>ALTITUD:</strong> 3337 m s. n. m..</small></p></li>
+          <li><p><small><strong>SUPERFICIE:</strong> 434.6 km².</small></p></li>
+          <li><p><small><strong>UTM:</strong> 8884642 266235 18L.</small></p></li>
+          <li><p><small><strong>HUSO HORARIO:</strong> UTC-5.</small></p></li>
+
+
+        </ul>
       <div id="map" class="map"> </div>
       </div>
     </div><!-- .row -->
