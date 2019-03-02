4c27813bdee6f42a2f7a997e2e96431ed35c7b92
favt12 <alexis_22_95@hotmail.com>
2019-03-01 23:25:09 -0500

    Se agregaron nuevas imagenes y se modifico la informacion de los slider que faltaban culminar en el archivo lugares-turisticos.php

--- includes/templates/lugares-turisticos.php
+++ includes/templates/lugares-turisticos.php
@@ -9,24 +9,15 @@
         <ol class="carousel-indicators">
           <li data-target="#slider-one" data-slide-to="0" class="active"></li>
           <li data-target="#slider-one" data-slide-to="1"></li>
-          <li data-target="#slider-one" data-slide-to="2"></li>
         </ol>
 
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="img/cococha.jpg" alt="imagen #1" class="d-block img-fluid" >
+            <img src="img/humanhueque.jpg" alt="imagen #1" class="d-block img-fluid" >
           </div><!-- .carousel-item .active -->
 
           <div class="carousel-item">
-            <img src="img/cococha.jpg" alt="imagen #2" class="d-block img-fluid">
-          </div><!-- .carousel-item -->
-
-          <div class="carousel-item">
-            <img src="img/cococha.jpg" alt="imagen #3" class="d-block img-fluid">
-            <div class="carousel-caption d-none d-md-block">
-              <h4>CONOCE LAS MEJORES PLAYAS</h4>
-              <p>Diversion Asegurada 100%</p>
-            </div>
+            <img src="img/lagunahuamanhuque.jpg" alt="imagen #2" class="d-block img-fluid">
           </div><!-- .carousel-item -->
        </div><!-- .carousel-inner -->
 
@@ -42,7 +33,7 @@
     </div><!-- .slider .col-12 .col-lg-7-->
 
     <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-      <h3>Laguna de Humanhueque</h3>
+      <h3>Laguna Humanhueque</h3>
       <p>Se encuentra en las partes altas de aquia a 4 horas a pie. Es uno de los lugares atractivos del distrito donde posee la abundancia de truchas en sus aguas turvias y el riego de sus alrededores.</p>
     </div>
 </div><!-- .row -->
@@ -90,56 +81,79 @@
 <div class="row">
   <div class="col-12 col-lg-7 slider">
 
-    <div class="carousel slide" id="slider-three" data-ride="carousel">
+    <div class="carousel slide" id="slider-six" data-ride="carousel">
       <ol class="carousel-indicators">
-        <li data-target="#slider-three" data-slide-to="0" class="active"></li>
-        <li data-target="#slider-three" data-slide-to="1"></li>
-        <li data-target="#slider-three" data-slide-to="2"></li>
+        <li data-target="#slider-six" data-slide-to="0" class="active"></li>
+        <li data-target="#slider-six" data-slide-to="1"></li>
       </ol>
 
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="img/aquia_hd.png" alt="imagen #1" class="d-block img-fluid" >
+          <img src="img/cocoche_aquia_vista.jpg" alt="imagen #1" class="d-block img-fluid" >
         </div><!-- .carousel-item .active -->
 
         <div class="carousel-item">
-          <img src="img/aquia_hd.png" alt="imagen #2" class="d-block img-fluid">
-        </div><!-- .carousel-item -->
-
-        <div class="carousel-item">
-          <img src="img/aquia_hd.png" alt="imagen #3" class="d-block img-fluid">
-          <div class="carousel-caption d-none d-md-block">
-            <h4>CONOCE LAS MEJORES PLAYAS</h4>
-            <p>Diversion Asegurada 100%</p>
-          </div>
+          <img src="img/cococha.jpg" alt="imagen #2" class="d-block img-fluid">
         </div><!-- .carousel-item -->
      </div><!-- .carousel-inner -->
 
-      <a href="#slider-three" class="carousel-control-prev" data-slide="prev">
+      <a href="#slider-six" class="carousel-control-prev" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Anterior</span>
       </a>
-      <a href="#slider-three" class="carousel-control-next" data-slide="next">
+      <a href="#slider-six" class="carousel-control-next" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Siguiente</span>
       </a>
     </div><!-- .carousel .slide #slider -->
-  </div><!-- .slider -->
-
-  <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-    <h3>Laguna Yanacocha</h3>
-    <p>Se encuentra en las pampas de lampas tambien ubicado en las partes altas del distrito rodeadas de flora y hermosos nevados.</p>
-  </div>
+    </div><!-- .slider -->
+      <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
+      <h3>Laguna Cococha</h3>
+      <p>Se encuentra en las pampas de lampas tambien ubicado en las partes altas del distrito rodeadas de flora y hermosos nevados.</p>
+    </div>
 </div><!-- .row -->
+
 
 <hr>
 
 <div class="row">
   <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-    <h3>Puya de Raymondi</h3>
-    <p>Recurso natural que se encuentra en el valle de Cara en el caserio de Pachapaqui.</p>
+      <h3>Plaza de Toros</h3>
+      <p>La plaza de toros de Aquia es sin duda una de las mas hermosa de la provincia de Bolognesi donde se lleva a cabo la corrida de toros, un espectaculo entretenido con una gran equipo de toreros profesionales que al sonido de la banda hace que este evento tenga un gran exito.</p>
   </div>
+  <div class="col-12 col-lg-7 slider">
+    <div class="carousel slide" id="slider-nine" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#slider-nine" data-slide-to="0" class="active"></li>
+        <li data-target="#slider-nine" data-slide-to="1"></li>
+      </ol>
 
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/toros_aquia.png" alt="imagen #1" class="d-block img-fluid" >
+        </div><!-- .carousel-item .active -->
+        <div class="carousel-item">
+          <img src="img/plaza_toros_lleno.jpg" alt="imagen #2" class="d-block img-fluid">
+          <div class="carousel-caption d-none d-md-block">
+          </div>
+        </div><!-- .carousel-item -->
+     </div><!-- .carousel-inner -->
+
+      <a href="#slider-nine" class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a href="#slider-nine" class="carousel-control-next" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Siguiente</span>
+      </a>
+    </div><!-- .carousel .slide #slider -->
+  </div><!-- .slider -->
+</div><!-- .row -->
+
+<hr>
+
+<div class="row">
   <div class="col-12 col-lg-7 slider">
 
     <div class="carousel slide" id="slider-seven" data-ride="carousel">
@@ -168,81 +182,8 @@
       </a>
     </div><!-- .carousel .slide #slider -->
   </div><!-- .slider -->
-</div><!-- .row -->
-
-<hr>
-
-<div class="row">
   <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-    <h3>Laguna Cococha</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-
-  <div class="col-12 col-lg-7 slider">
-
-    <div class="carousel slide" id="slider-six" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#slider-six" data-slide-to="0" class="active"></li>
-        <li data-target="#slider-six" data-slide-to="1"></li>
-      </ol>
-
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/cocoche_aquia_vista.jpg" alt="imagen #1" class="d-block img-fluid" >
-        </div><!-- .carousel-item .active -->
-
-        <div class="carousel-item">
-          <img src="img/cococha.jpg" alt="imagen #2" class="d-block img-fluid">
-        </div><!-- .carousel-item -->
-     </div><!-- .carousel-inner -->
-
-      <a href="#slider-six" class="carousel-control-prev" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
-      </a>
-      <a href="#slider-six" class="carousel-control-next" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Siguiente</span>
-      </a>
-    </div><!-- .carousel .slide #slider -->
-  </div><!-- .slider -->
-</div><!-- .row -->
-
-
-<hr>
-
-<div class="row">
-  <div class="col-12 col-lg-7 slider">
-
-    <div class="carousel slide" id="slider-nine" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#slider-nine" data-slide-to="0" class="active"></li>
-        <li data-target="#slider-nine" data-slide-to="1"></li>
-      </ol>
-
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/toros_aquia.png" alt="imagen #1" class="d-block img-fluid" >
-        </div><!-- .carousel-item .active -->
-        <div class="carousel-item">
-          <img src="img/plaza_toros_lleno.jpg" alt="imagen #2" class="d-block img-fluid">
-          <div class="carousel-caption d-none d-md-block">
-          </div>
-        </div><!-- .carousel-item -->
-     </div><!-- .carousel-inner -->
-
-      <a href="#slider-nine" class="carousel-control-prev" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
-      </a>
-      <a href="#slider-nine" class="carousel-control-next" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Siguiente</span>
-      </a>
-    </div><!-- .carousel .slide #slider -->
-  </div><!-- .slider -->
-  <div class="card-title  col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-    <h3>Plaza de Toros</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h3>Puya de Raymondi</h3>
+    <p>Recurso natural que se encuentra en el valle de Cara en el caserio de Pachapaqui.</p>
   </div>
 </div><!-- .row -->
