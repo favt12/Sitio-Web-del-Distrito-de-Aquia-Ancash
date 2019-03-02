dd4fc5a64045736e216e8cbf9ce91477e3e77fe7
favt12 <alexis_22_95@hotmail.com>
2019-03-01 23:19:29 -0500

    Se agregaron nuevas imagenes y se modifico la informacion de los slider que faltaban culminar en el archivo costumbres-danzas.php

--- includes/templates/costumbres-danzas.php
+++ includes/templates/costumbres-danzas.php
@@ -33,8 +33,10 @@
       </div><!-- .slider .col-12 .col-lg-7-->
 
       <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-        <h3>Fiesta a honor al patron San Miguel Arcangel</h3>
-        <p>Inicia el 27 de septiembre y culmina el 2 de Octubre</p>
+        <h3>Fiesta en honor  al patron San Miguel Arcangel</h3>
+        <p>Inicia el 27 de septiembre y culmina el 2 de Octubre llevando a cabo la tradicional fiesta costumbrista en honor al santo patron.</p>
+        <p>Los mayordomos reprensenta al kasike de un territorio determinado encargandose de realizar la misa  y dar alimentos a los visitantes y pobladores durante toda la fiesta y hacerlos bailer y sentirse comodo durante todo el evento.
+</p>
       </div>
   </div><!-- .row -->
 
@@ -42,9 +44,9 @@
 
   <div class="row">
     <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
-      <h3>Fiesta a Honor al Señor de Cayac</h3>
+      <h3>Fiesta en Honor al Señor de Cayac</h3>
       <p>Inicia la primera semana de Mayo siendo el dia central el 03 de dicho mes.
-        Cayac es un santuario a 15 minutos del pueblo de Aquia a orillas del rio, donde se puede apreciar al señor de Cayac que es una imagen de cristo representada en piedra que fue encontrada por un lugareño. En visperas de la fiesta los devotos van al santuario para la misa oficiada por el sacerdote, luego se lleva acabo la procesion y acto seguido los asistentes son invitados por los mayordomos para una cena y luego continuar con la festividad con sus musicas costumbristas.
+        Cayac es un santuario a 15 minutos del pueblo de Aquia a orillas del rio, donde se puede apreciar al señor de Cayac que es una imagen de cristo representada en piedra que fue encontrada por un lugareño. En visperas de la fiesta los devotos van al santuario para la misa oficiada por el sacerdote, luego se lleva acabo la procesion y acto seguido los asistentes son invitados por los mayordomos para una cena y luego continuar con la festividad.
       </p>
     </div>
 
@@ -58,11 +60,11 @@
 
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="img/cayac_misa1.jpg" alt="imagen #1" class="d-block img-fluid" >
+            <img src="img/cayac_misa.jpg" alt="imagen #1" class="d-block img-fluid" >
           </div><!-- .carousel-item .active -->
 
         <div class="carousel-item">
-            <img src="img/cayac1.jpg" alt="imagen #2" class="d-block img-fluid">
+            <img src="img/santuario.jpg" alt="imagen #2" class="d-block img-fluid">
           </div><!-- .carousel-item -->
        </div><!-- .carousel-inner -->
 
@@ -87,24 +89,15 @@
         <ol class="carousel-indicators">
           <li data-target="#slider-four" data-slide-to="0" class="active"></li>
           <li data-target="#slider-four" data-slide-to="1"></li>
-          <li data-target="#slider-four" data-slide-to="2"></li>
         </ol>
 
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="img/aquia_hd.png" alt="imagen #1" class="d-block img-fluid" >
+            <img src="img/castillo.jpg" alt="imagen #1" class="d-block img-fluid" >
           </div><!-- .carousel-item .active -->
 
           <div class="carousel-item">
-            <img src="img/aquia_hd.png" alt="imagen #2" class="d-block img-fluid">
-          </div><!-- .carousel-item -->
-
-          <div class="carousel-item">
-            <img src="img/aquia_hd.png" alt="imagen #3" class="d-block img-fluid">
-            <div class="carousel-caption d-none d-md-block">
-              <h4>CONOCE LAS MEJORES PLAYAS</h4>
-              <p>Diversion Asegurada 100%</p>
-            </div>
+            <img src="img/ano_nuevo_negritos.jpg" alt="imagen #2" class="d-block img-fluid">
           </div><!-- .carousel-item -->
        </div><!-- .carousel-inner -->
 
@@ -121,7 +114,7 @@
 
     <div class="card-title col-12 col-lg-5 text-justify d-flex align-items-center flex-wrap">
       <h3>Fiesta a honor al Niño Jesus</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Es un fiesta bastante celebrada por los pobladores, inicia el 30 diciembre al 02 de enero teniendo en particularidad mostrar la danza de los negritos durante todo la celebracion en homenaje al Niño Jesus, acompañado de la orquesta que hace bailar a toda la poblacion motivando en seguir la costumbre todos los años.</p>
     </div>
   </div><!-- .row -->
 
