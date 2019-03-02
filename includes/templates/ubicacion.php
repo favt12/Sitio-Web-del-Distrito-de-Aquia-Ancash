e6a3b6266fc51052ef75248aef54aafb9b0b2807
favt12 <alexis_22_95@hotmail.com>
2019-03-01 23:10:10 -0500

    Se agrego texto y se modifico el boton con enlace externo en la parte posterior de la img

--- includes/templates/ubicacion.php
+++ includes/templates/ubicacion.php
@@ -10,12 +10,15 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h3 class="text-center mb-5">UBICACION DE AQUIA</h3>
-          <div class="row no-gutters mb-3">
+        <h3 class="mb-3">UBICACION DE AQUIA</h3>
+          <div class="row no-gutters">
+            <p>Para llegar al distrito de Aquia desde Lima, existen dos rutas: Una es la que parte del km 183 de la Panamericana Norte (Paramonga) y tomar el desvío de la carretera que va hacia Huaraz, a la altura de Conococha tomar el desvío que llega hasta Chiquián y termina en el pueblo de Aquia.
+                La otra ruta también tiene el mismo recorrido desde la Panamericana Norte (Paramonga) hasta Conococha solo que de ahí se debe tomar la carretera que va a Antamina y hasta el cruce llamado por los pobladores de la zona como BY PASS de ahí tomar el desvío para racrachacra la cual lleva directo hasta Aquia en apenas 15 minutos.</p>
             <div class="col-md-7">
-              <img src="img/map_ruta_aquia.png" class="card-img rounded border" alt="Ruta">
+              <img src="img/map_ruta_aquia.png" class="card-img rounded-top border mb-2" alt="Ruta">
+              <a href="https://www.google.com/maps/@-10.0724349,-77.1528275,15.25z" class="btn btn-success btn-block btn-sm" target="_blank">Ver Mapa</a>
             </div>
-            <div class="col-md-5">
+            <div class="col-md-5 ">
                 <ul class="ml-5 mt-5">
                   <li><p><small><strong>PAIS:</strong> Perú.</small></p></li>
                   <li><p><small><strong>REGION:</strong> Ancash.</small></p></li>
@@ -26,9 +29,6 @@
                   <li><p><small><strong>UTM:</strong> 8884642 266235 18L.</small></p></li>
                   <li><p><small><strong>HUSO HORARIO:</strong> UTC-5.</small></p></li>
                 </ul>
-                <div class="row justify-content-center">
-                  <a href="https://www.google.com/maps/@-10.0724349,-77.1528275,15.25z" class="btn btn-success mt-5" target="_blank">Ver Mapa</a>
-                </div><!-- .row .justify-content-center -->
             </div><!-- .col-md-5 -->
           </div><!-- .row .no-gutters .mb-3 -->
         <hr>
