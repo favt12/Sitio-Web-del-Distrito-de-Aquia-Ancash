f8393c0b3aa5925a3ed8af2b90829d28e516e239
favt12 <alexis_22_95@hotmail.com>
2019-03-17 11:22:08 -0500

    Se agrego una seccion contacto en el footer.php

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,23 +1,44 @@
 <footer>
   <div class="container-fluid">
-    <div class="footer row text-center">
-      <div class="col-4">
-        <h4>Corporation Telvas</h4>
-        <h6 class="text-uppercase">contacto: 947391408</h6>
+    <div class="footer row text-center justify-content-center">
+      <div class="col-8">
+        <h6 class="h1 mt-5">CONTACTO</h6>
+        <form action="" class="formulario mt-5">
+            <div class="form-group row">
+              <div class="col-12 col-md-6">
+                <input type="text" placeholder="Nombre" name="" id="">
+            </div><!-- col-12 col-md-6 -->
 
-      </div>
-      <div class="col-4">
-        <h6>COPYRIGHT © 2019</h6>
-      </div>
-      <div class="sociales col-4">
-        <a href="#"><i class="icon-facebook"></i></a>
-        <a href="#"><i class="icon-twitter"></i></a>
-        <a href="#"><i class="icon-github-circled"></i></a>
-        <a href="#"><i class="icon-instagram"></i></a>
+            <div class="col-12 col-md-6">
+                  <input type="email" placeholder="Email" name="" id="">
+            </div><!-- col-12 col-md-6 -->
 
-      </div>
+            </div><!-- form-group row -->
+            <textarea name="" id="" placeholder="Mensaje..."></textarea>
+            <div class="form-group d-flex justify-content-center">
+              <a href="#" class="btn btn-success btn-lg" target="_blank">Enviar</a>
+            </div><!-- .form-group .d-flex .justify-content-center -->
+          </form>
+        </div><!-- .col-8 -->
 
-    </div>
+        <div class="w-100"></div>
 
-  </div>
+        <div class="col-4 mt-5">
+          <h4>Corporation Telvas</h4>
+        </div><!-- .col-4 mt-5-->
+
+        <div class="col-4 mt-5">
+          <h6>COPYRIGHT © 2019</h6>
+          <h6><small>Designed & Developed by Telvas</small></h6>
+        </div><!-- .col-4 .mt-5 -->
+
+        <div class="sociales col-4 mt-5">
+          <!-- <a href="#"><i class="icon-facebook"></i></a> -->
+          <a href="#"><i class="icon-twitter"></i></a>
+          <a href="#"><i class="icon-github-circled"></i></a>
+          <!-- <a href="#"><i class="icon-instagram"></i></a> -->
+        </div><!-- .sociales .col-4 .mt-5 -->
+
+    </div><!-- .footer .row .text-center .justify-content-center -->
+  </div><!-- .container-fluid -->
 </footer>
