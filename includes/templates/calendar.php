492c6f446785f78bb9296d0f3b74c0780cc5b4fe
camilo12 <alexisvasquestello@gmail.com>
2019-03-23 18:04:11 -0500

    Se modificaron las fechas de los carnavales en el arch. calendar.php

--- includes/templates/calendar.php
+++ includes/templates/calendar.php
@@ -4,7 +4,7 @@
     <div class="col-12 col-md-4 d-flex align-items-center">
   <div class="">
     <ul>
-      <li><p><small><strong>05 al 08 de Marzo</strong> Carnavales </small></p></li>
+      <li><p><small><strong>02 al 04 de Marzo</strong> Carnavales </small></p></li>
       <li><p><small><strong>3 de Mayo</strong> Celebracion en Honor al Señor de Cayac </small></p></li>
       <li><p><small><strong>27 de Septiembre al 02 de Octubre</strong> Fiesta costumbrista a San Miguel Arcangel</small></p></li>
       <li><p><small><strong>30 de Diciembre al 02 de Enero</strong> Fiesta honor al niño Jesùs en Año Nuevo</small></p></li>
