763c993ce10ad77a1b61939233ff35a997845a94
camilo12 <alexisvasquestello@gmail.com>
2019-03-24 16:46:14 -0500

    Se inserto la propiedad data-width='100%' en el arch. comments.php pluginFacebook Responsive

--- includes/templates/comments.php
+++ includes/templates/comments.php
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-12">
       <div id="fb-root"></div>
-      <div class="fb-comments" data-href="http://localhost/aquia_page_oficial/" data-numposts="5"></div>
+      <div class="fb-comments" data-href="http://localhost/aquia_page_oficial/" data-width="100%" data-numposts="5"></div>
     </div>
   </div>
 </div>
