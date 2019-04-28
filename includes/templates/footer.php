64767fbe75b3c75d8e33507a40e0436a878b55d8
favt12 <alexis_22_95@hotmail.com>
2019-04-28 11:57:05 -0500

    Se separo el footer y el formulario en dferentes secciones dentro del archivo footer.php

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,12 +1,16 @@
-<footer>
+<div class="form-backg">
   <div class="container-fluid">
-    <div class="footer row text-center justify-content-center">
+    <div class="row text-center justify-content-center">
       <div class="col-8">
         <h6 class="h1 mt-5">CONTACTO</h6>
         <form method="POST" action="https://formspree.io/alexis_22_95@hotmail.com" class="formulario mt-5">
             <div class="form-group row">
               <div class="col-12 col-md-6">
-                <input type="text" placeholder="Nombre y Apellidos" name="name" pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" required>
+                <input type="text" placeholder="Nombres" name="name" pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" required>
+            </div><!-- col-12 col-md-6 -->
+            
+            <div class="col-12 col-md-6">
+                <input type="text" placeholder="Apellidos" name="name" pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" required>
             </div><!-- col-12 col-md-6 -->
 
             <div class="col-12 col-md-6">
@@ -20,25 +24,28 @@
             </div><!-- .form-group .d-flex .justify-content-center -->
           </form>
         </div><!-- .col-8 -->
+    </div><!-- .row .text-center .justify-content-center -->
+   </div><!-- .container --> 
+</div>
 
-        <div class="w-100"></div>
+<footer>
+  <div class="container">
+    <div class="row text-center justify-content-center">
+      <div class="col-4 mt-5">
+        <h4 class="color-footer"><small>Company</small> | Telvas<img src="img/logo_telvas.png" alt="" width="60px"></h4>
+      </div><!-- .col-4 mt-5-->
 
-        <div class="col-4 mt-5">
-          <h4><small>Company</small> | Telvas</h4>
-        </div><!-- .col-4 mt-5-->
+      <div class="col-4 mt-5">
+        <h6 class="color-footer">COPYRIGHT © <small class="Footer-year"></small></h6>
+        <h6 class="color-footer"><small>Designed & Developed by Telvas</small></h6>
+      </div><!-- .col-4 .mt-5 -->
 
-        <div class="col-4 mt-5">
-          <h6>COPYRIGHT © <small class="Footer-year"></small></h6>
-          <h6><small>Designed & Developed by Telvas</small></h6>
-        </div><!-- .col-4 .mt-5 -->
-
-        <div class="sociales col-4 mt-5">
-          <!-- <a href="#"><i class="icon-facebook"></i></a> -->
-          <a href="#"><i class="icon-twitter"></i></a>
-          <a href="#"><i class="icon-github-circled"></i></a>
-          <!-- <a href="#"><i class="icon-instagram"></i></a> -->
-        </div><!-- .sociales .col-4 .mt-5 -->
-
-    </div><!-- .footer .row .text-center .justify-content-center -->
-  </div><!-- .container-fluid -->
+      <div class="sociales col-4 mt-5">
+        <!-- <a href="#"><i class="icon-facebook"></i></a> -->
+        <a href="#"><i class="icon-twitter"></i></a>
+        <a href="#"><i class="icon-github-circled"></i></a>
+        <!-- <a href="#"><i class="icon-instagram"></i></a> -->
+      </div><!-- .sociales .col-4 .mt-5 -->
+    </div><!-- .row .text-center .justify-content-center -->
+  </div><!-- .container --> 
 </footer>
